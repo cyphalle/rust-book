04e979749ebb00a77a65a3a918d252748ce1954b
User <user@example.com>
2025-07-20 19:40:29 +0000

    chap2 user input

---
+++ chap_2/guessing_game/target/debug/deps/guessing_game-d8edb7fab4d33732.d
@@ -0,0 +1,9 @@
+/root/rust-book/chap_2/guessing_game/target/debug/deps/guessing_game-d8edb7fab4d33732.d: src/main.rs Cargo.toml
+
+/root/rust-book/chap_2/guessing_game/target/debug/deps/libguessing_game-d8edb7fab4d33732.rmeta: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
---
+++ chap_2/guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/root/rust-book/chap_2/guessing_game/target/debug/guessing_game: /root/rust-book/chap_2/guessing_game/src/main.rs
